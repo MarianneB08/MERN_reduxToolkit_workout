fa7addcc1e0ca021b7db90d872b9ef9dd5a3839a
MarianneB08 <mboyer.correction.relecture@gmail.com>
2023-04-25 18:39:35 +0200

    Backend : ajout du package cors // Frontend : mise en place de React, création du composant NewPost.js et de sa logique pour indiquer le pseudo de l'utilisateur et poster un nouveau message dans la DB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const port = 3000;
 
 // Connexion à la DB
 connectDB();
 
 const app = express();
+
+// Autorisation CORS
+app.use(cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+    optionsSuccessStatus: 200,
+}))
 
 // Middleware qui permet de traiter les données de la requête
 app.use(express.json());
---
+++ frontend/src/components/NewPost.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const NewPost = ({ userId }) => {
+  const [message, setMessage] = useState("");
+
+  // Fonction exécutée au clic sur le bouton "Envoyer" du formulaire
+  const handleForm = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3000/post/", {
+      message,
+      author: userId,
+    });
+    // Remise à zéro du state message
+    setMessage("");
+  };
+
+  return (
+    <div>
+      <form className="new-post-container" onSubmit={(e) => handleForm(e)}>
+        <textarea
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Quoi de neuf ?"
+          value={message}
+        ></textarea>
+        <input type="submit" value="Envoyer" />
+      </form>
+    </div>
+  );
+};
+
+export default NewPost;
